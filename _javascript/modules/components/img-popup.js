6fc21b84a4adf4e3f77dacbdb9ffd46e97b38b3d
Shinminjin <tlsalswls123@gmail.com>
2024-04-17 20:55:47 +0900

    fix: ImagePopup 관련 배포오류 해결

--- _javascript/modules/components/img-popup.js
+++ _javascript/modules/components/img-popup.js
@@ -1,22 +1,22 @@
-/**
- * Set up image popup
- *
- * See: https://github.com/dimsemenov/Magnific-Popup
- */
+// /**
+//  * Set up image popup
+//  *
+//  * See: https://github.com/dimsemenov/Magnific-Popup
+//  */
 
-export function imgPopup() {
-  if ($('.popup') <= 0) {
-    return;
-  }
+// export function imgPopup() {
+//   if ($('.popup') <= 0) {
+//     return;
+//   }
 
-  $('.popup').magnificPopup({
-    type: 'image',
-    closeOnContentClick: true,
-    showCloseBtn: false,
-    zoom: {
-      enabled: true,
-      duration: 300,
-      easing: 'ease-in-out'
-    }
-  });
-}
+//   $('.popup').magnificPopup({
+//     type: 'image',
+//     closeOnContentClick: true,
+//     showCloseBtn: false,
+//     zoom: {
+//       enabled: true,
+//       duration: 300,
+//       easing: 'ease-in-out'
+//     }
+//   });
+// }
